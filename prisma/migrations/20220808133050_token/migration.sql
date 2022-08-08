7ca999ab3edac31fdba3e3d2e27ee5b34a61a389
eduardoNagashima <eduyudji@gmail.com>
2022-08-08 10:31:29 -0300

    manual test of create users and manage token

---
+++ prisma/migrations/20220808133050_token/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "posts" ADD COLUMN     "coverImg" TEXT NOT NULL DEFAULT 'https://5287aa00874a313e299d-1850966fc307ff23e1e789aeafd2476b.ssl.cf5.rackcdn.com/PostImagem/42240/foto-branca-download-wallpapers-e-onde-usar_o1fcm9aiph1mte1km1f11cs53mng.png';
