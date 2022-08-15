4e83769047c2bb5fa6e127f083caa926defb34ce
eduardoNagashima <eduyudji@gmail.com>
2022-08-15 16:43:37 -0300

    final commit version 0.1

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -4,7 +4,7 @@ import { userData } from "../services/userService.js";
 export const signUpSchema: ObjectSchema<userData> = Joi.object({
     email: Joi.string().email().required(),
     username: Joi.string().required(),
-    password: Joi.string().min(10).required(),
+    password: Joi.string().required(),
     confirmPassword: Joi.string().required().valid(Joi.ref('password')),
     image: Joi.string().regex(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/)
 })
