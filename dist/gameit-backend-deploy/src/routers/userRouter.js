86b15e6009f670a4fbddd13de741381824344100
eduardoNagashima <eduyudji@gmail.com>
2022-08-16 13:31:18 -0300

    try to solve deploy

--- dist/gameit-backend-deploy/src/routers/userRouter.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { signIn, signUp } from "../controllers/userController.js";
-var userRouter = Router();
-userRouter.post('/signup', signUp);
-userRouter.post('/signIn', signIn);
-export default userRouter;
